c69f0bda577c8728a03375051c1d3d89970fec6a
diegoritu <49616722+diegoritu@users.noreply.github.com>
2019-09-07 12:54:16 -0300

    InquilinoDAO terminado

--- API_TP/src/controlador/Controlador.java
+++ API_TP/src/controlador/Controlador.java
@@ -33,6 +33,8 @@ public class Controlador {
 		return instancia;
 	}
 	
+	private Persona aBuscar;
+	
 	public List<EdificioView> getEdificios(){
 		List<EdificioView> resultado = new ArrayList<EdificioView>();
 		List<Edificio> edificios = new EdificioDAO().getEdificios();
@@ -202,13 +204,12 @@ public class Controlador {
 	}	
 	
 	private Persona buscarPersona(String documento) {
-		Persona aBuscar = null; //NO HAY FORMA DE INICIALIZARLO COMO PERSONA, Y DEBE INICIALIZARSE
 		try {
 			aBuscar = new PersonaDAO().findById(documento);
 		} catch (PersonaException e) {
 			e.printStackTrace();
 		}
-		return aBuscar;				
+		return aBuscar;			
 	}
 	
 	private Reclamo buscarReclamo(int numero) throws ReclamoException {
