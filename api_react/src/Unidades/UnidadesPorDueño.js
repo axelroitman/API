11f38ceaf90f20e8be5f7414fe0e24a35522c072
diegoritu <49616722+diegoritu@users.noreply.github.com>
2019-12-01 19:19:55 -0300

    UnidadesPorDueño

--- api_react/src/Unidades/UnidadesPorDueño.js
+++ api_react/src/Unidades/UnidadesPorDueño.js
@@ -33,27 +33,32 @@ class UnidadesPorDueño extends Component {
       var  {isLoaded, unidades, cargado} = this.state;
 
     if(!isLoaded) {
-        return <div>Cargando...</div>
+        return <div className='container'>Cargando...</div>
      }   
      else if (unidades.length == 0)
      {
-        return(
-          <p>No hay propiedades para mostrar.</p>
-        );
+      return(
+        <div className='container'><p>No hay propiedades para mostrar.</p></div>
+      );
      }
      else{
       return (
          
-          <div>
-             <h1>Mis Propiedades</h1>
-
-              <ul className="listUnidades">
+          <div className='container'>
+             <h2>Mis Propiedades</h2>
+             <div className="tabla">
+              <table>
+                    <tr>
+                    <th>Edificio</th>
+                    <th>Piso</th>
+                    <th>N°</th>
+                    </tr>
              {
                unidades.map(item => { 
-                return <li key={item.id}> {item.edificio.nombre}, {item.piso}° {item.numero}</li>
-              })
+               return <tr> <td>{item.edificio.nombre}</td><td>{item.piso}</td><td>{item.numero}</td> </tr>})
              }
-              </ul>
+              </table>
+              </div>
              </div>
 
             );
