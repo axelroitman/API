d4b754c0ff8197876aa59be411d30a132d1824c8
Axel <axelr@live.com.ar>
2019-12-01 22:52:00 -0300

    Header

--- api_react/src/Header.js
+++ api_react/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import {Bootstrap, Grid, Row, Col} from 'react-bootstrap';
 
 class Header extends React.Component {
 
@@ -23,15 +24,18 @@ class Header extends React.Component {
         return (
           <div className="topBlue">
               <h1 onClick={this.handlePageChange}>Claimer</h1>
-             <ul className="menu" >
-                <li  style={{ listStyleType: "none" }}>
-                  <Link to="/personas"><h3>Personas</h3></Link>
-                </li>
-                <li  onClick={this.handlerClickItem.bind()} style={{ listStyleType: "none" }}>
-                <Link to=""><h3>Cerrar sesión</h3></Link>
-              </li>
+             <div className="menu" >
+                  <div className="menuItem menuItemIzq">
+                  <h3>Hola {sessionStorage.getItem("nombre")}</h3>
+                  </div>
+                  <div className="menuItem">
+                    <Link to="/"><h3>Panel de administración</h3></Link>
+                  </div>
+                  <div className="menuItem menuItemDer" onClick={this.handlerClickItem.bind()} style={{ listStyleType: "none" }}>
+                    <Link to=""><h3>Cerrar sesión</h3></Link>
+                  </div>
 
-              </ul>
+              </div>
               <hr/>
           </div>
          );
@@ -41,28 +45,31 @@ class Header extends React.Component {
         return (
           <div className="topBlue">
               <h1 onClick={this.handlePageChange}>Claimer</h1>
-             <ul className="menu" >
-              <li  style={{ listStyleType: "none" }}>
-                <Link to="/"><h3>Reclamos</h3></Link>
-              </li>
-              <li  style={{ listStyleType: "none" }}>
-                <Link to="/nuevo-reclamo"><h3>Nuevo reclamo</h3></Link>
-              </li>
-              <li  style={{ listStyleType: "none" }}>
-                <Link to="/unidadesAlquiladas"><h3>Mis Alquileres</h3></Link>
-              </li>
-              <li  style={{ listStyleType: "none" }}>
-                <Link to="/unidadesPropias"><h3>Mis Propiedades</h3></Link>
-              </li>
-              <li  style={{ listStyleType: "none" }}>
-                <Link to="/recEdif"><h3>Reclamos en mis edificios</h3></Link>
-              </li>
+              <div className="menu" >
+                  <div className="menuItem menuItemIzq">
+                  <h3>Hola {sessionStorage.getItem("nombre")}</h3>
+                  </div>
+                  <div className="menuItem">
+                  <Link to="/"><h3>Reclamos</h3></Link>
+                  </div>
+                  <div className="menuItem">
+                  <Link to="/nuevo-reclamo"><h3>Nuevo reclamo</h3></Link>
+                  </div>
+                  <div className="menuItem">
+                  <Link to="/unidadesAlquiladas"><h3>Mis Alquileres</h3></Link>
+                  </div>
+                  <div className="menuItem">
+                  <Link to="/unidadesPropias"><h3>Mis Propiedades</h3></Link>
+                  </div>
+                  <div className="menuItem">
+                  <Link to="/recEdif"><h3>Reclamos en mis edificios</h3></Link>
+                  </div>
 
-              <li  onClick={this.handlerClickItem.bind()} style={{ listStyleType: "none" }}>
-                <Link to=""><h3>Cerrar sesión</h3></Link>
-              </li>
+                  <div className="menuItem menuItemDer" onClick={this.handlerClickItem.bind()} style={{ listStyleType: "none" }}>
+                    <Link to=""><h3>Cerrar sesión</h3></Link>
+                  </div>
 
-            </ul>
+              </div>
                 <hr/>
          </div>
          );
